a024bb483966280fc9e24fa302926c00de9b455b
AGS48353 <naruedon.wat@gmail.com>
2020-08-24 16:36:43 +0700

    added redirect from base larevel page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/user_infos');
 });
 // routes 
 Route::get('/user_infos', 'userinfoscontroller@index');
