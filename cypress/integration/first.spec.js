db2c9aae8301c6369b1b8d71142ece55d0307bbb
AGS48353 <naruedon.wat@gmail.com>
2020-08-28 16:13:42 +0700

    cypress for testing

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -1,14 +1,18 @@
 context('Actions', () => {
-    beforeEach(() => {
-        cy.visit('http://127.0.0.1:8000/')
-        cy.get('#username')
-            .type('jacobson.pablo')
-            .should('have.value', 'jacobson.pablo')
-        cy.get('#password')
-            .type('12345678ABC')
-            .should('have.value', '12345678ABC')
-        cy.get('#loginbtn').click()
+    before(() => {
+        cy.login().visit('/user_infos')
     })
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('laravel_session', 'XSRF-TOKEN')
+
+        // cy.get('#username')
+        //     .type('jacobson.pablo')
+        //     .should('have.value', 'jacobson.pablo')
+        // cy.get('#password')
+        //     .type('12345678ABC')
+        //     .should('have.value', '12345678ABC')
+        // cy.get('#loginbtn').click()
+    });
 
     // it('enters username', () => {
     //     cy.get('#username')
@@ -29,6 +33,31 @@ context('Actions', () => {
     //         .should('have.value', '12345678ABC')
     //     cy.get('#loginbtn').click()
     // })
+
+    it('goes in and leaves and logs in properlly', () => {
+        cy.wait(2000)
+        cy.get('#yeet').click()
+        cy.get('#username')
+            .type('jacobson.pablo')
+            .should('have.value', 'jacobson.pablo')
+        cy.get('#password')
+            .type('12345678ABC')
+            .should('have.value', '12345678ABC')
+        cy.get('#loginbtn').click()
+    });
+    it('cy.window() - get the global window object', () => {
+        // https://on.cypress.io/window
+        cy.window().should('have.property', 'top')
+    });
+    it('cy.document() - get the document object', () => {
+        // https://on.cypress.io/document
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    });
+
+    it('cy.title() - get the title', () => {
+        // https://on.cypress.io/title
+        cy.title().should('include', 'User Informations')
+    });
     it('changes the search terms', () => {
         cy.get('#searchfrom').select('Name').should('have.value', '1')
         cy.get('#myInputTextField').type('Joan')
@@ -43,15 +72,15 @@ context('Actions', () => {
         cy.get('#users-table tr').should('contain', 'hessel.elody')
 
         cy.get('#searchfrom').select('Phone').should('have.value', '4')
-        cy.get('#myInputTextField').type('7076')
-        cy.get('#users-table tr').should('contain', '7076')
+        cy.get('#myInputTextField').type('4235')
+        cy.get('#users-table tr').should('contain', '4235')
 
         cy.get('#searchfrom').select('Email').should('have.value', '5')
-        cy.get('#myInputTextField').type('jimmie.harris@example.net')
-        cy.get('#users-table tr').should('contain', 'jimmie.harris@example.net')
-    })
-    it('Adds someone & deletes it LOL', () => {
-        cy.wait(1000)
+        cy.get('#myInputTextField').type('huels')
+        cy.get('#users-table tr').should('contain', 'huels.rodrick@example.net')
+    });
+    it('Adds someone & deletes it', () => {
+        cy.wait(2000)
         cy.get('#addbtn').click()
 
         cy.get('#eName').type('cytest')
@@ -62,17 +91,17 @@ context('Actions', () => {
         cy.get('#ePass').type('cytest123')
         cy.get('#ePassC').type('cytest123')
         cy.get('#eSave').click()
-        cy.get('#myInputTextField').type('cytest')
+        cy.get('#myInputTextField').type('{selectall}{del}cytest')
 
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('#users-table tr').get('#editbtn').click()
 
         cy.get('#eDel').click()
         cy.get('#eDel2').click()
-        cy.wait(1000)
-    })
-    it('Adds someone & edits it LOL', () => {
-        cy.wait(1000)
+        cy.wait(2000)
+    });
+    it('Adds someone & edits it', () => {
+        cy.wait(2000)
         cy.get('#addbtn').click()
 
         cy.get('#eName').type('cytest')
@@ -83,41 +112,36 @@ context('Actions', () => {
         cy.get('#ePass').type('cytest123')
         cy.get('#ePassC').type('cytest123')
         cy.get('#eSave').click()
-        cy.get('#myInputTextField').type('cytest')
+        cy.get('#myInputTextField').type('{selectall}{del}cytest')
 
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('#users-table tr').get('#editbtn').click()
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('#eName').type('edit')
         cy.get('#eSurName').type('edit')
         cy.get('#eUser').type('edit')
         cy.get('#ePhone').type('{selectall}{backspace}555555555')
         cy.get('#eEmail').type('{selectall}{del}newcymail@mailedcy.cy')
         cy.get('#eSave').click()
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('#searchfrom').select('Name').should('have.value', '1')
-        cy.get('#myInputTextField').type('cytest')
+        cy.get('#myInputTextField').type('{selectall}{del}cytest')
         cy.get('#users-table tr').should('contain', 'cytestedit')
 
-    })
+    });
 
-    it('finds itself and deletes it' ,()=> {
+    it('finds itself and deletes it', () => {
         cy.get('#searchfrom').select('Name').should('have.value', '1')
-        cy.get('#myInputTextField').type('cytest')
-        cy.wait(1000)
+        cy.get('#myInputTextField').type('{selectall}{del}cytest')
+        cy.wait(2000)
         cy.get('#users-table tr').get('#editbtn').click()
         cy.get('#eDel').click()
         cy.get('#eDel2').click()
-        cy.wait(1000)
-    })
+        cy.wait(2000)
+    });
 
-    it('goes in and leaves',()=>{
-        cy.wait(1000)
-        cy.get('#yeet').click()
-    })
-
-    it('leaves to register and leave to remove regiestered acc',()=>{
-        cy.wait(1000)
+    it('leaves to register and leave to remove regiestered acc', () => {
+        cy.wait(2000)
         cy.get('#yeet').click()
         cy.visit('http://127.0.0.1:8000/register')
         cy.get('#name').type('cytest')
@@ -129,7 +153,7 @@ context('Actions', () => {
         cy.get('#password-confirm').type('cytest123')
         cy.get('#register').click()
 
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('#yeet').click()
 
         cy.visit('http://127.0.0.1:8000/')
@@ -142,13 +166,13 @@ context('Actions', () => {
         cy.get('#loginbtn').click()
 
         cy.get('#searchfrom').select('Name').should('have.value', '1')
-        cy.get('#myInputTextField').type('cytest')
-        cy.wait(1000)
+        cy.get('#myInputTextField').type('{selectall}{del}cytest')
+        cy.wait(2000)
         cy.get('#users-table tr').get('#editbtn').click()
         cy.get('#eDel').click()
         cy.get('#eDel2').click()
-        cy.wait(1000)
+        cy.wait(2000)
 
         cy.get('#yeet').click()
-    })
+    });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (username = null, password = null) => {
+    username = username || Cypress.env('username')
+    password = password || Cypress.env('password')
+    let _token
+    cy.visit('/login')
+        .getCookie('XSRF-TOKEN')
+        .then((cookie) => {
+            cy.request({
+                method: 'POST',
+                url: '/login',
+                form: true,
+                body: {
+                    username,
+                    password,
+                },
+                headers: {
+                    // Base64 encoded string was URI encoded in headers. Decode it.
+                    'X-XSRF-TOKEN': decodeURIComponent(cookie.value)
+                }
+            });
+        });
+})
